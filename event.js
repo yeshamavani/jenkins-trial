7e4323ae84f79c51d7bc8d57277bd6130ed8370e
Yesha Mavani <yesha.mavani@sourcefuse.com>
2025-01-22 19:11:51 +0530

    initial

--- event.js
+++ event.js
@@ -1,5 +1,8 @@
 console.log("Hello, world! from eventttt");
 
+const eventDetails = { ...process.env };
+console.log("Event details:---- yesha", eventDetails);
+
 const jobStatus = process.env.JOB_STATUS;
 const jobName = process.env.JOB_NAME;
 const buildNumber = process.env.BUILD_NUMBER;
